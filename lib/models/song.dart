eadff5070d0993e568f3968ef44f7402fb7962d3
yor878 <yorleygarciagarcia31@gmail.com>
2026-07-08 11:46:09 -0600

    Create song.dart

---
+++ lib/models/song.dart
@@ -0,0 +1,41 @@
+import '../models/song.dart';
+
+List<Song> songs = [
+
+Song(
+id: "1",
+title: "Bohemian Rhapsody",
+artist: "Queen",
+album: "A Night at the Opera",
+genre: "Rock",
+year: 1975,
+),
+
+Song(
+id: "2",
+title: "Billie Jean",
+artist: "Michael Jackson",
+album: "Thriller",
+genre: "Pop",
+year: 1982,
+),
+
+Song(
+id: "3",
+title: "Smells Like Teen Spirit",
+artist: "Nirvana",
+album: "Nevermind",
+genre: "Grunge",
+year: 1991,
+),
+
+Song(
+id: "4",
+title: "Shape of You",
+artist: "Ed Sheeran",
+album: "Divide",
+genre: "Pop",
+year: 2017,
+),
+
+];
