f31bb49c75b70261589adcf329a786b82bab5759
yor878 <yorleygarciagarcia31@gmail.com>
2026-07-08 11:45:07 -0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+class Song {
+  String id;
+  String title;
+  String artist;
+  String album;
+  String genre;
+  int year;
+
+  Song({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.album,
+    required this.genre,
+    required this.year,
+  });
+}
